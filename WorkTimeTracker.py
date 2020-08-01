31641d6b4b2a7a1a556d646d2890492b134624de
reallarry <67504977+reallarry@users.noreply.github.com>
2020-08-01 11:09:26 +0000

    Update WorkTimeTracker.py
    Changed the position of the client_name and task_name variables

--- WorkTimeTracker.py
+++ WorkTimeTracker.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 # Import Libraries
 from datetime import datetime
 import csv
-
-client_name = input("Please enter the name of the client: \n")
-task_name = input("Please enter the name of the task you are undertaking: \n")
 
 # Set the date of entry as the system's current date
 current_date = datetime.now().date()
 
 # Take users input on the start and stop times
+client_name = input('Please enter the name of the client : ')
+task_name = input('Please enter the name of the task you are undertaking : ')
 start_time = input('Enter the time you started in the format Hour:Minute AM : ')
 stop_time = input('Enter the time you ended in the format Hour:Minute PM : ')
 time_format = '%I:%M %p' # this is the Hour:Minute AM/PM format
-
 
 # Convert the string format time entered by the user into datetime 
 # format using the datetime.strptime()
